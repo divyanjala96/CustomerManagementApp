eabddea3d81f7f9a13d39da798f0cb30db7e4ae9
Your Name <shameeindrajith1995@gmail.com>
2021-01-18 15:09:09 +0530

    product controll updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Category;
+use domain\Facades\ProductFacade;
+use domain\Facades\CategoryFacade;
 
 class ProductController extends ParentController
 {
@@ -12,40 +14,31 @@ class ProductController extends ParentController
 //* open product page
 public function index()
 {
-    $response['categaries']=Category::all();
-    $response['products']=Product::all();
+    $response['categaries']=CategoryFacade::getAllData();
+    $response['products']=ProductFacade::getAllData();
     return view('product')->with($response);
 }
 
 //* product save function
 public function proStore(Request $request)
 {
-    $product = new Product;
-    $product->cat_id=$request->category;
-    $product->pro_name=$request->pro_name;
-    $product->pro_price=$request->pro_price;
-
-    $image=$request->file('file');
-    $image_name=time().'.'.$image->extension();
-    $image->move(public_path('images'),$image_name);
-    $product->image=$image_name;
-    $product->save();
+    ProductFacade::store($request);
     return redirect()->back();
 }
 
 //* product delete function
 public function deleteProduct($id)
 {
-    $delete=Product::find($id);
+    $delete=ProductFacade::getProductData($id);
     $delete->delete();
     return redirect()->back();
 }
 
 //* product update function start
 public function updateProduct($id)
-{
-    $response['categaries']=Category::all();
-    $response['products']=Product::find($id);
+{   $update=ProductFacade::getProductData($id);
+    $response['categaries']=CategoryFacade::getAllData();
+    $response['products']=ProductFacade::getProductData($id);
     return view('productUpdate')->with($response);
 
 }
@@ -53,22 +46,9 @@ public function updateProduct($id)
 public function update(Request $update)
 {
     $id=$update->id;
-    $name=$update->name;
-    $price=$update->price;
-    $cat_id=$update->category;
-
-    $update_pro_data=Product::find($id);
-    $update_pro_data->pro_name=$name;
-    $update_pro_data->pro_price=$price;
-    $update_pro_data->cat_id=$cat_id;
-
-    $image=$update->file('file');
-    $image_name=time().'.'.$image->extension();
-    $image->move(public_path('images'),$image_name);
-    $update_pro_data->$image=$image_name;
-
-    $update_pro_data->save();
-
+    $product_data=ProductFacade::getProductData($id);
+    ProductFacade::store($product_data);
+    
     return redirect(route('product-all'));
 
 }
